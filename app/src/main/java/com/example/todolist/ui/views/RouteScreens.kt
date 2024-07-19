d6b62ffd215c721465064a5e20cf5255ad758a3c
= <=>
2024-07-19 17:32:54 +0000

    TODOLIST

---
+++ app/src/main/java/com/example/todolist/ui/views/RouteScreens.kt
@@ -0,0 +1,27 @@
+package com.example.todolist.ui.views
+
+
+sealed class AuthRouteScreens(val path: String) {
+
+    data object RegistrationScreen : AuthRouteScreens("registration")
+    data object SignInScreen : AuthRouteScreens("sign-in")
+    data object ForgottenPasswordScreen : AuthRouteScreens("forgotten-password")
+    data object OTPCodeScreen : AuthRouteScreens("otp-code")
+    data object NewPasswordPasswordScreen : AuthRouteScreens("new-password-password")
+
+}
+
+sealed class StartRouteScreens(val path: String) {
+
+    data object StartScreens : StartRouteScreens("start")
+}
+
+sealed class MainRouteScreens(val path: String) {
+
+    data object HomeScreen : MainRouteScreens("home")
+
+}
+
+
+//class HomeScreen {}
+
